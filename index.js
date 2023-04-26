9b5041bfa593e04ab8f6a0559611f31f807a8ce5
ali taslimpoor <shayannniiii@gmail.com>
2023-04-26 17:04:57 +0300

    solve assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+
+var customerName = "bob";
+function getCustomerName() {
+  return customerName;
+}
+
+console.log(getCustomerName()); 
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+        bestCustomer = "not bob";
+      }
+      
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "mamad";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "asghar"
+}
+        
+
+        
+          
+
+
